4213d19176b9cda4ee037d8900ceb2bf2a684798
rachna rajak <113829584+rachnarajaka040@users.noreply.github.com>
2023-08-05 13:38:14 +0530

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Image1 from "../assets/images1.png";
-
+import Image13 from "../assets/image13.jpg";
+import vector from "../assets/vector.svg";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -25,34 +26,34 @@ export default function Home() {
   };
   return (
     <div>
-      <div className="flex p-8 w-[70%] m-auto " >
-        <div className="w-[530px] h-[563px]  m-auto gap-10" >
-          <div className="flex-1 h-[300px] w-[370px]">
+      <div className="flex p-8 w-[100%]  " style={{justifyContent:"space-between"}} >
+        <div className="w-[400] h-[563px]" style={{marginLeft:"150px",paddingTop:"60px"}}>
+          <div className="flex-1 h-[500px] w-[500px]">
             <h1
-              className="text-3xl font-bold mb-4 text-5xl"
+              className=" font-bold mb-4 text-6xl"
               style={{
                 color: "#34251F",
                 fontWeight: "700px",
-                lineHeight: "55px",
+                lineHeight: "57px",
                 fontFamily: "Frank Ruhl Libre",
               }}
             >
-              Find The Best Fashion Trend For You
+              Find The Best <br></br> Fashion Trend <br></br> For You
             </h1>
             <p
               className=""
               style={{
                 color: "rgba(52, 37, 31, 1)",
-                fontSize: "18px",
+                fontSize: "20px",
                 fontFamily: "Lato",
               }}
             >
-              A Saree is The Perfect Way Of Proudly Flaunting Who i Am Without To
+              A Saree is The Perfect Way Of Proudly Flaunting <br></br> Who i Am Without To
               Say It. <br></br>The Saree Makes Woman Look Preety Yet <br></br>{" "}
               Graceful All At The Same Time
             </p>
             <button
-              className="text-white font-bold py-2 px-4 rounded shadow-md mt-8"
+              className="text-white font-bold py-3 px-9 rounded shadow-md mt-16"
               style={{
                 backgroundColor: "#34251F",
                 opacity: "90%",
@@ -64,27 +65,27 @@ export default function Home() {
             </button>
           </div>
         </div>
-        <div>
+        
           <div
-            className="w-[400px]  h-[350px] relative shadow-[5px_5px_0px_0px_rgba(109,40,217)]  "
-            style={{ backgroundColor: "rgba(216, 219, 81, 0.72)", paddingRight: "-900px" }}
+            className="w-[600px]  h-[580px] relative shadow-[5px_5px_0px_0px_rgba(109,40,217)]"
+            style={{ backgroundColor: "rgba(216, 219, 81, 0.72)"}}
           >
             {/* Right Images */}
             <Image
               src={Image1}
               alt="Image 1"
-
-
-              className="mb-4 absolute left-[-67px] bottom-[-60px] h-[430px]"
-
+              
+            
+              className="mb-4 absolute left-[-115px]  top-[-30px]"
+              style={{width:"100%",height:"670px",}}
             />
           </div>
-        </div>
+        
       </div>
 
       {/* New div with the requested structure */}
 
-      <div className="w-[70%] h-[500px]  flex flex-col  m-auto" style={{ marginTop: "-150px" }} >
+      <div className="w-[70%] h-[500px] relative  flex flex-col  m-auto" style={{ marginTop: "0px" }} >
         <div>
           <h1
             className="text-2xl font-bold mb-4 text-center"
@@ -103,7 +104,7 @@ export default function Home() {
             <div className="flex mt-4 w-[900px] justify-center  gap-5">
               {/* Div 1 */}
               <div
-                className="flex flex-col justify-center p-4 h-[350px] w-[290px]"
+                className="flex flex-col justify-center p-4 h-[400px] w-[300px]"
                 style={{ backgroundColor: "#E5E7EB" }}
               >
                 {/* Add content for the first div */}
@@ -118,7 +119,7 @@ export default function Home() {
 
               {/* Div 2 */}
               <div
-                className="flex flex-col justify-center p-4 h-[350px] w-[290px]"
+                className="flex flex-col justify-center p-4 h-[400px] w-[300px]"
                 style={{ backgroundColor: "#E5E7EB" }}
               >
                 {/* Add content for the first div */}
@@ -133,7 +134,7 @@ export default function Home() {
 
               {/* Div 3 */}
               <div
-                className="flex flex-col justify-center p-4 h-[350px] w-[290px]"
+                className="flex flex-col justify-center p-4 h-[400px] w-[300px]"
                 style={{ backgroundColor: "#E5E7EB" }}
               >
                 {/* Add content for the first div */}
@@ -150,7 +151,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className=" w-[70%] m-auto relative" style={{ marginTop: '-100px' }}>
+      <div className=" w-[100%] m-auto relative" style={{ marginTop: '-150px' }}>
         {/* Yellow Circles */}
         <div
           className="rounded-full  opacity-0.5 h-96 w-96 absolute top-[-50px] left-[-50px]"
@@ -168,7 +169,8 @@ export default function Home() {
 
       </div>
 
-      <div className="w-[92%] h-[450px] flex justify-center gap-20 items-center bg-yellow-600" style={{ marginTop: "250px", marginLeft: "4%", marginRight: "4%" }}>
+      <div className="w-[92%] h-[450px] flex justify-center gap-20 items-center bg-yellow-600" style={{ marginTop: "250px", marginLeft: "4%", marginRight: "4%", backgroundImage: "url('vector')" }}>
+
         {/* Left Side */}
         <div className="flex flex-col items-center">
           {/* Small Circle */}
@@ -209,11 +211,11 @@ export default function Home() {
       <div className="w-[80%] h-[900px] m-auto mt-8 flex flex-col items-center" >
         {/* Heading */}
         <h2 className="text-3xl font-bold mb-4" style={{ color: "#34251F", fontFamily: "Frank Ruhl Libre", fontSize: "40px" }}>Our Product</h2>
-        <div className="flex justify-center gap-10 w-full mb-4">
-          <NavLink title="Best Seller" />
-          <NavLink title="On Sell" />
-          <NavLink title="Trending Now" />
-          <NavLink title="New Arrival" />
+        <div className="flex justify-center gap-10 w-full mb-4" style={{fontWeight:"bold"}} >
+          <NavLink title="MOST LOVED" />
+          <NavLink title="SALE" />
+          <NavLink title="TRENDING " />
+          <NavLink title="NEW ARRIVAL" />
         </div>
         {/* First Row */}
         <div className="flex justify-center gap-5 w-full mb-4">
@@ -269,8 +271,72 @@ export default function Home() {
           </div>
         </div>
       </div>
+  
+
+<div className="w-[80%] h-[70%] m-auto  bg-yellow-600" style={{ marginTop: "-30px", marginBottom: "25px", border: "2px solid black" }}>
+  <Image src={Image13} alt="Image 13" style={{ width:"100%", height:"70%", objectFit: "cover" }} />
+ 
+</div>
 
 
+
+<div className="w-[80%] h-[900px] m-auto mt-8 flex flex-col items-center" >
+        {/* Heading */}
+       
+        {/* First Row */}
+        <div className="flex justify-center gap-5 w-full mb-4">
+          {/* Product 1 */}
+          <div className=" bg-gray-100 shadow-md rounded-md p-4 " style={{ width: "250px", height: "350px" }}>
+            {/* Product Image */}
+            <Image src={Image1} alt="Image 1" className="mb-4" />
+          </div>
+
+          {/* Product 2 */}
+          <div className="w-40 h-40 bg-gray-100 shadow-md rounded-md p-4 " style={{ width: "250px", height: "350px" }}>
+            {/* Product Image */}
+            <Image src={Image1} alt="Image 1" className="mb-4" />
+          </div>
+
+          {/* Product 3 */}
+          <div className="w-40 h-40 bg-gray-100 shadow-md rounded-md p-4 " style={{ width: "250px", height: "350px" }}>
+            {/* Product Image */}
+            <Image src={Image1} alt="Image 1" className="mb-4" />
+          </div>
+
+          {/* Product 4 */}
+          <div className="w-40 h-40 bg-gray-100 shadow-md rounded-md p-4 " style={{ width: "250px", height: "350px" }}>
+            {/* Product Image */}
+            <Image src={Image1} alt="Image 1" className="mb-4" />
+          </div>
+        </div>
+
+        {/* Second Row */}
+        <div className="flex justify-center gap-5 w-full mb-4">
+          {/* Product 5 */}
+          <div className="w-40 h-40 bg-gray-100 shadow-md rounded-md p-4 " style={{ width: "250px", height: "350px" }}>
+            {/* Product Image */}
+            <Image src={Image1} alt="Image 1" className="mb-4" />
+          </div>
+
+          {/* Product 6 */}
+          <div className="w-40 h-40 bg-gray-100 shadow-md rounded-md p-4 " style={{ width: "250px", height: "350px" }}>
+            {/* Product Image */}
+            <Image src={Image1} alt="Image 1" className="mb-4" />
+          </div>
+
+          {/* Product 7 */}
+          <div className="w-40 h-40 bg-gray-100 shadow-md rounded-md p-4 " style={{ width: "250px", height: "350px" }}>
+            {/* Product Image */}
+            <Image src={Image1} alt="Image 1" className="mb-4" />
+          </div>
+
+          {/* Product 8 */}
+          <div className="w-40 h-40 bg-gray-100 shadow-md rounded-md p-4 " style={{ width: "250px", height: "350px" }}>
+            {/* Product Image */}
+            <Image src={Image1} alt="Image 1" className="mb-4" />
+          </div>
+        </div>
+      </div>
 
     </div>
   );
