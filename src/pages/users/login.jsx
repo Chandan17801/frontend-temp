80db4f851ffba4c4dd51a1f436e30ac42cd9a6c6
surendraSinghKamboj <surendra.singh.kamboj@hotmail.com>
2023-08-14 14:33:17 +0530

    updates

--- src/pages/users/login.jsx
+++ src/pages/users/login.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const Login = () => {
   const [loginData, setLoginData] = useState({});
@@ -15,11 +18,29 @@ const Login = () => {
     console.log("Submited");
   };
   return (
-    <section>
-      <form>
-        <input type="text" name="email" onChange={handleChnage} />
-        <input type="password" name="password" onChange={handleChnage} />
-        <input type="submit" value="Login" />
+    <section className=" min-h-screen flex justify-center items-center absolute top-0 left-0 w-full bg bg-transparent">
+      <form
+        onSubmit={handleSubmit}
+        className={`flex flex-col justify-center items-center gap-6 bg-[#A89011C7] p-10 rounded-xl ${inter.className}`}
+      >
+        <h6 className="text-white text-xl">Login</h6>
+        <input
+          className="rounded-lg px-2 "
+          type="text"
+          name="email"
+          onChange={handleChnage}
+        />
+        <input
+          className="rounded-lg px-2 "
+          type="password"
+          name="password"
+          onChange={handleChnage}
+        />
+        <input
+          className="rounded-lg px-2 bg-[#CFC77A] text-white text-lg cursor-pointer hover:opacity-90"
+          type="submit"
+          value="Login"
+        />
       </form>
     </section>
   );
