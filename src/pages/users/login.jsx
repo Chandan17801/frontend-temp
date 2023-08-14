bdba06fc73b2e19fa6f0a092a75498a4d16bf427
surendraSinghKamboj <surendra.singh.kamboj@hotmail.com>
2023-08-14 08:38:27 +0530

    login file elements created

--- src/pages/users/login.jsx
+++ src/pages/users/login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const login = () => {
+  const [loginData, setLoginData] = useState({});
+  // Chnage handler
+  const handleChnage = (e) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  //   submit Handler
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submited");
+  };
   return (
-    <div>login</div>
-  )
-}
+    <section>
+      <form>
+        <input type="text" name="email" onChange={handleChnage} />
+        <input type="password" name="password" onChange={handleChnage} />
+        <input type="submit" value="Login" />
+      </form>
+    </section>
+  );
+};
 
-export default login
+export default login;
