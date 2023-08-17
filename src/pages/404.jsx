872bee9e117a4b764ba78e2010a1209372382faa
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2023-08-17 22:55:46 +0530

    404page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+Link;
 
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <div>
+      <h3>This Page is not available</h3>
+      <button>Redirection to home page </button>
+      <Link>landin</Link>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
