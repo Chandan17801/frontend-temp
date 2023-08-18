66ccf5b54ef8e0a008b505fd980282dfb76baaa4
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2023-08-18 14:35:28 +0530

    fix logo

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -21,20 +21,19 @@ const Error = () => {
   }, [seconds, router]);
 
   return (
-    <div className="flex items-center  min-h-screen bg-yellow">
-      <div className="w-full max-w-screen-xl mx-auto p-4 sm:p-8  text-center flex sm:flex-row flex-col items-center">
-        <div className="sm:w-1/2 sm:pr-4 ">
+    <div className="flex items-center min-h-screen bg-yellow">
+      <div className="w-full max-w-screen-xl mx-auto p-4 sm:p-8 text-center flex flex-col sm:flex-row items-center">
+        <div className="sm:w-1/2 sm:pr-4">
           <h2 className="text-3xl font-bold text-yellow-600 sm:text-2xl mt-2 sm:mt-4">
-            Apologize,
+            Sorry!
           </h2>
           <p className="text-yellow-600 font-bold text-2xl mt-2">
-            We have a problem.
+            Requested Page Not Found.
           </p>
-          <div className="text-opacity-80  text-gray-700">
+          <div className="text-opacity-80 text-gray-700">
             <h1 className="text-xl font-bold text-9xl">404</h1>
           </div>
-
-          <div className="mt-4 flex justify-center ">
+          <div className="mt-4 flex justify-center">
             <a
               href="/"
               className="shadow-md inline-block px-4 py-2 bg-yellow-500 rounded-lg text-white font-bold flex items-center space-x-1 hover:bg-white-600"
@@ -44,7 +43,7 @@ const Error = () => {
             </a>
           </div>
         </div>
-        <div className="sm:w-1/2 flex justify-center items-center mt-4 sm:mt-0">
+        <div className="sm:w-1/2 flex justify-center items-center mt-4 sm:mt-0 order-first sm:order-last">
           <Image src={logo} alt="Logo" width={300} height={300} />
         </div>
       </div>
