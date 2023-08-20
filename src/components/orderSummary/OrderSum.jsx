52ad91d8555a157a2ed88a662a1df8839f8b3f73
surendraSinghKamboj <surendra.singh.kamboj@hotmail.com>
2023-08-20 13:09:02 +0530

    order page calulations

--- src/components/orderSummary/OrderSum.jsx
+++ src/components/orderSummary/OrderSum.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaMinus, FaPlus, FaArrowLeft } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const Ship = () => {
   const [sareeCount1, setSareeCount1] = useState(0);
   const [sareeCount2, setSareeCount2] = useState(0);
+  const [subtotal, setSubtotal] = useState(0);
+  const [tax, setTax] = useState(0);
+  const [total, setTotal] = useState(0);
 
+  const cartData = useSelector((state) => state.cart);
   const incrementSaree = (section) => {
+    // dummy code
     if (section === 1) {
       setSareeCount1(sareeCount1 + 1);
     } else if (section === 2) {
@@ -21,54 +27,53 @@ const Ship = () => {
     }
   };
 
+  // original code
+  console.log(cartData);
+
+  useEffect(() => {
+    let sub_total = 0;
+    cartData.forEach(({ price }) => {
+      return (sub_total += price);
+    });
+    setSubtotal(sub_total);
+    setTax((sub_total / 100) * 12);
+    setTotal(sub_total + tax);
+  }, [cartData]);
+
   return (
     <div className="w-full md:w-1/2  ml-4 md:ml-10 mr-4 md:mr-10 mt-10 md:pb-10">
       <div className="flex flex-col md:flex-row">
         <FaArrowLeft className="mr-2" />
         <h2 className="pl-2 md:pl-4">Order Summary</h2>
       </div>
-      <div className="flex m-6 items-center">
-        <div className="bg-gray-200 h-20 w-20"></div>
-        <div>
-          <h2 className="ml-6">Saree</h2>
-          <div className="ml-4 mt-4 items-center">
-            <button
-              className="text-gray-400 bg-gray-200"
-              onClick={() => decrementSaree(1)}
-            >
-              <FaMinus />
-            </button>
-            <span className="mx-2">{sareeCount1}</span>
-            <button
-              className="text-gray-400 bg-gray-200"
-              onClick={() => incrementSaree(1)}
-            >
-              <FaPlus />
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="flex m-6 items-center">
-        <div className="bg-gray-200 h-20 w-20"></div>
-        <div>
-          <h2 className="ml-6">Saree</h2>
-          <div className="ml-4 mt-4 items-center">
-            <button
-              className="text-gray-400 bg-gray-200"
-              onClick={() => decrementSaree(2)}
-            >
-              <FaMinus />
-            </button>
-            <span className="mx-2">{sareeCount2}</span>
-            <button
-              className="text-gray-400 bg-gray-200"
-              onClick={() => incrementSaree(2)}
-            >
-              <FaPlus />
-            </button>
-          </div>
-        </div>
-      </div>
+      {cartData &&
+        cartData.map((item) => {
+          return (
+            <div className="flex m-6 items-center" key={item._id}>
+              <div className="bg-gray-200 h-20 w-20">
+                <img src={item.image} alt={item.name} />
+              </div>
+              <div>
+                <h2 className="ml-6">{item.name}</h2>
+                <div className="ml-4 mt-4 items-center">
+                  <button
+                    className="text-gray-400 bg-gray-200"
+                    onClick={() => decrementSaree(1)}
+                  >
+                    <FaMinus />
+                  </button>
+                  <span className="mx-2">{item.quantity}</span>
+                  <button
+                    className="text-gray-400 bg-gray-200"
+                    onClick={() => incrementSaree(1)}
+                  >
+                    <FaPlus />
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       <div className=" md:ml-2 md:ml-15 items-center mt-6 md:mt-0">
         <div className="flex justify-between p-6">
           <p className=" md:mb-0 ">Gift Card/Discount Code</p>
@@ -82,14 +87,14 @@ const Ship = () => {
       <div className="ml-4 md:ml-20 mt-6 md:mt-0">
         <div className="flex justify-between p-6">
           <div className="">Subtotal</div>
-          <div>12,000.00</div>
+          <div>{subtotal}</div>
         </div>
         <hr className="border-t border-black-500 w-full ml-2" />
       </div>
       <div className="ml-4 md:ml-20 mt-6 md:mt-0">
         <div className="flex justify-between p-6">
-          <div className="">Sales tax(6.5%)</div>
-          <div>40.23</div>
+          <div className="">Sales tax(12%)</div>
+          <div>{tax}</div>
         </div>
         <hr className="border-t border-black-500 w-full ml-2" />
       </div>
@@ -103,7 +108,7 @@ const Ship = () => {
       <div className="bg-gray-700 border-4  ml-4 md:ml-20 mt-6 md:mt-0">
         <div className=" flex justify-between p-6">
           <div className="font-bold text-yellow-500">Total </div>
-          <div className="font-bold text-yellow-500">12,130.23</div>
+          <div className="font-bold text-yellow-500">{total}</div>
         </div>
       </div>
     </div>
--- store/features/cart.js
+++ store/features/cart.js
@@ -1,6 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = [
+  { _id: "64e1653d2b7c4412dc5d00af",image:"http://localhost:4001/files/uploads/img3.jpeg",name:"Kanchipuram Saree", quantity: 1, price: 2999 },
+  { _id: "64e082a95d007620e05e13b4",image:"http://localhost:4001/files/uploads/img2.jpeg",name:"Banarasi Saree", quantity: 1, price: 2999 },
+];
 
 const cart = createSlice({
   name: "cart",
